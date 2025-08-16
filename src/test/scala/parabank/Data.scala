95d6662f75931c91b0d059339dcd0a70d1fc66d3
Isabel838 <isabelcristinamejialopez@gmail.com>
2025-08-16 09:49:13 -0500

    Update Data.scala

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -4,7 +4,11 @@ object Data{
     val url= "https://parabank.parasoft.com/parabank/services/bank"
     val username = "Pedrop"
     val password = "1234"
+    val fromAccountId = "17118"
+    val toAccountId = "19449"
+    val amount = "10"
 
     
 }
+
 
